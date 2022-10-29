68b3d03bdd309e922c305ec00f31ec37123b1880
Simon Peshkov <s_peshkov@list.ru>
2022-10-30 01:01:25 +0700

    Autorisation

---
+++ cypress/e2e/Autorisation_Mobile.cy.js
@@ -0,0 +1,24 @@
+describe('Autorisation', function (){
+
+    const USER_LOGIN='god';
+
+    it('Autorisation', function () {
+
+        cy.visit('https://platform.betskilla.com/')
+        cy.viewport('iphone-xr')
+
+        cy.get('#input-Connexion').type(USER_LOGIN)
+            .should('have.value',USER_LOGIN)
+
+        cy.get("input[type='password']").type('god')
+
+
+        cy.get('button[class="sc-evZas gLjLbw MuiButtonBase-root sc-breuTD kIbPdU MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium"]').should('be.visible').click()
+
+        cy.get('button[class="sc-evZas gLjLbw MuiButtonBase-root sc-dIouRR dhumOf MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium"]',{timeout: 30000}).should('be.visible').click()
+
+        cy.get('#button_settings').click()
+
+
+    });
+})
